be5a5c075ac6839d9dfd02efe05afc55ab023b78
Kushaj Arora <kushaj.arora@optum.com>
2025-08-18 16:35:31 +0530

    Initial commit

---
+++ src/main/java/com/blogwebsite/training_blog_website/TrainingBlogWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.blogwebsite.training_blog_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingBlogWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingBlogWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blogwebsite/training_blog_website/TrainingBlogWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogwebsite.training_blog_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingBlogWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
