87de47f85e8f3aed447ad140074256d34857c42c
Kushaj Arora <kushaj.arora@optum.com>
2025-08-18 18:08:37 +0530

    Blog Model added

---
+++ src/main/java/com/blogwebsite/training_blog_website/entity/BlogModel.java
@@ -0,0 +1,30 @@
+package com.blogwebsite.training_blog_website.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BlogModel {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	private String content;
+	
+	@NotBlank
+	private String author;
+	
+	@NotBlank
+	private LocalDateTime createdAt;
+}
