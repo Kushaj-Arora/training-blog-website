ae36ad09e0477c92188f7eb8de4ea2437373a20e
Kushaj Arora <kushaj.arora@optum.com>
2025-08-19 14:31:27 +0530

    Mysql DB connection completed

--- src/main/java/com/blogwebsite/training_blog_website/entity/BlogModel.java
+++ src/main/java/com/blogwebsite/training_blog_website/entity/BlogModel.java
@@ -16,6 +16,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name="blogs")
 public class BlogModel {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -33,34 +34,4 @@ public class BlogModel {
     protected void onCreate() {
         this.createdAt = LocalDateTime.now();
     }
-
-    // --- Getters ---
-//    public String getContent() {
-//        return content;
-//    }
-//
-//    public String getAuthor() {
-//        return author;
-//    }
-//
-//    public LocalDateTime getCreatedAt() {
-//        return createdAt;
-//    }
-//
-//    public Long getId() {
-//    	return id;
-//    }
-//
-//    // --- Setters ---
-//    public void setContent(String content) {
-//        this.content = content;
-//    }
-//
-//    public void setAuthor(String author) {
-//        this.author = author;
-//    }
-//
-//    public void setCreatedAt(LocalDateTime createdAt) {
-//        this.createdAt = createdAt;
-//    }
 }
