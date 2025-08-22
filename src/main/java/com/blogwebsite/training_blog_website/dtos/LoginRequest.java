14603167cb3607ae43f082b8bd1456c074d82185
Kushaj Arora <kushaj.arora@optum.com>
2025-08-22 11:38:17 +0530

    Login and signUp request DTO and entity added

---
+++ src/main/java/com/blogwebsite/training_blog_website/dtos/LoginRequest.java
@@ -0,0 +1,17 @@
+package com.blogwebsite.training_blog_website.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LoginRequest {
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/blogwebsite/training_blog_website/dtos/SignUpRequest.java
@@ -0,0 +1,7 @@
+package com.blogwebsite.training_blog_website.dtos;
+
+public class SignUpRequest {
+    private String username;
+    private String email;
+    private String password;
+}
